a616d50d5c2eab713c5dbeea2d8553e867478212
DancerCube <357633047@qq.com>
2015-06-01 16:14:00 +0800

    import js

--- js/javascript.js
+++ js/javascript.js
@@ -2,11 +2,14 @@
 * @Author: anchen
 * @Date:   2015-06-01 15:51:04
 * @Last Modified by:   anchen
-* @Last Modified time: 2015-06-01 15:55:29
+* @Last Modified time: 2015-06-01 16:13:09
 */
 
 'use strict';
 var oDiv = document.getElementById('div1');
-oDiv.onclick = function(){
+oDiv.onmouseover = function(){
+    this.style.background = 'green';
+}
+oDiv.onmouseout = function(){
     this.style.background = 'red';
 }
