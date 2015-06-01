7b30b0452cada13df74e9449089e17c6e0de616a
DancerCube <357633047@qq.com>
2015-06-01 15:56:14 +0800

    all commit

---
+++ js/javascript.js
@@ -0,0 +1,12 @@
+/* 
+* @Author: anchen
+* @Date:   2015-06-01 15:51:04
+* @Last Modified by:   anchen
+* @Last Modified time: 2015-06-01 15:55:29
+*/
+
+'use strict';
+var oDiv = document.getElementById('div1');
+oDiv.onclick = function(){
+    this.style.background = 'red';
+}
